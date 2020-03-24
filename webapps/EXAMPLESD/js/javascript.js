94b1669b1bedb1b90f0a4ffc7b7d5b4d658b1cfe
Alex V Eustrop <eustrop@eustrosoft.org>
2020-03-25 01:16:43 +0300

    (minor) setQR(id,qr) implemented for next commit

--- webapps/EXAMPLESD/js/javascript.js
+++ webapps/EXAMPLESD/js/javascript.js
@@ -11,3 +11,7 @@ function newQR(qr) {
     let qrfield = document.getElementById("qrcode");
     qrfield.value = qr;
 }
+function setQR(field_id,qr) { // SIC! а вообще это надо переделать
+    let qrfield = document.getElementById(field_id);
+    qrfield.value = qr;
+}
